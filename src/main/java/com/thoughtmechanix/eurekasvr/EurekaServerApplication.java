e64e0ca1e704e459aa38c8c361b79580aa8cdafd
supermoon <supermoon@nexon.co.kr>
2019-06-27 14:47:49 +0900

    first discovery

---
+++ src/main/java/com/thoughtmechanix/eurekasvr/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.thoughtmechanix.eurekasvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
