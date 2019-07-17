afc3d2ec4e0b28d4f4b046a42b13e847232d5780
supermoon <supermoon@nexon.co.kr>
2019-07-17 10:49:41 +0900

    profile test

--- src/main/java/com/thoughtmechanix/eurekasvr/EurekaServerApplication.java
+++ src/main/java/com/thoughtmechanix/eurekasvr/EurekaServerApplication.java
@@ -1,13 +1,29 @@
 package com.thoughtmechanix.eurekasvr;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication extends SpringBootServletInitializer {
+
+    @Value("${spring.profiles}")
+    String profiles;
+
+    @Value("${server.port}")
+    String port;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("profiles : " + profiles);
+        System.out.println("port : " + port);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
